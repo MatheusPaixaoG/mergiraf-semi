9ebaba4987633fdd39bf90da87367b6165e6bc04
Matheus Paixão Gumercindo dos Santos <mpgs@cin.ufpe.br>
2025-12-08 23:56:34 -0300

    change the expected output of Authenticator test case

--- examples/swift/Authenticator/expected.swift
+++ examples/swift/Authenticator/expected.swift
@@ -3,19 +3,11 @@ struct User {
 }
 
 class Authenticator {
-<<<<<<< LEFT
-    func authenticate(username: String, password: String) -> Bool {
-        // check username and password
-        return username == "admin" && password == "s3cr3t"
-    }
-=======
-    func authenticate(user: String) -> User? {
-        // modern token-based auth (returns a user on success)
-        if user == "admin" {
+    func authenticate(username: String, password: String) -> User? {
+        if username == "admin" && password == "s3cr3t" {
             return User(name: "admin")
         } else {
             return nil
         }
     }
->>>>>>> RIGHT
 }
